59f86fe881330a1f467759cbd5badc76b98b2626
Bagas <banabda@gmail.com>
2020-04-29 09:35:18 +0700

    fetching data from firebase with Future Builder

--- lib/screen/editpage.dart
+++ lib/screen/editpage.dart
@@ -1,15 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:khatam_counter/services/auth.dart';
+import 'package:provider/provider.dart';
 
-class EditPage extends StatelessWidget {
+import '../user.dart';
+
+class EditPage extends StatefulWidget {
+  @override
+  _EditPageState createState() => _EditPageState();
+}
+
+class _EditPageState extends State<EditPage> {
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<User>(context);
     return Scaffold(
       backgroundColor: Color(0xff423FA5),
-      body: Center(
-        child: MaterialButton(
-          onPressed: () => authService.googleSignIn()
-        )
+      body: FutureBuilder(
+        initialData: Firestore.instance.collection("user/${user.uid}/progress").getDocuments(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasData) {
+              if (snapshot.data!=null) {
+                return MaterialButton(
+                onPressed: () => authService.googleSignIn(),
+                child: Text(snapshot.data.documents.toList()[0].data["ayat"].toString()),);
+              }
+            } else {
+              return Text("kosooongngggg");
+            }
+            return Text("testing..");
+        },
       ),
     );
   }
